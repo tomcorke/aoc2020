de48abe766481095c6fdb4699b993140d27553bd
Tom Corke <tom.corke@skyscanner.net>
2020-12-17 15:37:58 +0000

    export all the things

--- src/17/index.ts
+++ src/17/index.ts
@@ -9,16 +9,18 @@ import { Solution } from "..";
 
 const getInput = readFileSeparated("\n", "17", "input");
 
-type Grid = string[][][];
-
-const add = (a: number[], b: number[]) => {
+export const addArrays = (a: number[], b: number[]) => {
   if (a.length !== b.length) {
     throw Error("Mismatched array length");
   }
   return a.map((an, i) => an + b[i]);
 };
 
-const getRange = (dimensions: number, min: number, max: number): number[][] => {
+export const getRange = (
+  dimensions: number,
+  min: number,
+  max: number
+): number[][] => {
   const range: number[][] = [];
   for (let i = min; i <= max; i++) {
     if (dimensions > 1) {
@@ -31,7 +33,7 @@ const getRange = (dimensions: number, min: number, max: number): number[][] => {
   return range;
 };
 
-const getDimensionalRange = (
+export const getDimensionalRange = (
   dimensions: number,
   min: number[],
   max: number[]
@@ -52,7 +54,7 @@ const getDimensionalRange = (
   return range;
 };
 
-class HyperGrid<T> {
+export class HyperGrid<T> {
   private dimensions: number;
   private defaultValue: T;
   private subGrids = new Map<number, HyperGrid<T>>();
@@ -106,18 +108,35 @@ class HyperGrid<T> {
     }
   }
 
-  getAdjacents(coords: number[]) {
+  *getAdjacents(coords: number[]) {
     if (coords.length < this.dimensions) {
       throw Error("Coords length must match dimensions");
     }
     const deltas = getRange(this.dimensions, -1, 1).filter(
       (d) => !d.every((n) => n === 0)
     );
-    return deltas.map((delta) => this.get(add(coords, delta)));
+    const results: T[] = [];
+    for (let delta of deltas) {
+      yield this.get(addArrays(coords, delta));
+    }
   }
 
-  countAdjacents(coords: number[], selector: (value: T) => boolean) {
-    return this.getAdjacents(coords).filter(selector).length;
+  countAdjacents(
+    coords: number[],
+    selector: (value: T) => boolean,
+    limit?: number
+  ) {
+    const adjacents = this.getAdjacents(coords);
+    let count = 0;
+    for (let adjacent of adjacents) {
+      if (selector(adjacent)) {
+        count++;
+        if (limit && count >= limit) {
+          break;
+        }
+      }
+    }
+    return count;
   }
 
   count(selector: (value: T | undefined) => boolean): number {
@@ -219,169 +238,7 @@ class HyperGrid<T> {
   }
 }
 
-const prettyPrint = (grid: Grid) => {
-  for (let z of grid) {
-    for (let y of z) {
-      console.log(y.join(""));
-    }
-    console.log("");
-  }
-  console.log("-");
-  console.log("");
-};
-
-const flatPrint = (grid: Grid) => {
-  let values: string[] = [];
-  for (let z of grid) {
-    for (let y of z) {
-      values.push(y.join(""));
-    }
-  }
-  console.log(values.join(""));
-};
-
-const clone = (grid: Grid): Grid => {
-  return JSON.parse(JSON.stringify(grid)) as Grid;
-};
-const cloneAndExpand = (grid: Grid): Grid => {
-  let newGrid = clone(grid);
-
-  let ySize = grid[0].length;
-  let xSize = grid[0][0].length;
-
-  // expand x right
-  if (newGrid.some((y) => y.some((x) => x[xSize - 1] === "#"))) {
-    newGrid = newGrid.map((z) => z.map((y) => [...y, "."]));
-  }
-
-  // expand x left
-  if (newGrid.some((y) => y.some((x) => x[0] === "#"))) {
-    newGrid = newGrid.map((z) => z.map((y) => [".", ...y]));
-  }
-
-  xSize = newGrid[0][0].length;
-
-  // expand y up
-  if (newGrid.some((y) => y[0].some((x) => x === "#"))) {
-    newGrid = newGrid.map((y) => [Array(xSize).fill("."), ...y]);
-    ySize = newGrid[0].length;
-  }
-
-  // expand y down
-  if (newGrid.some((y) => y[ySize - 1].some((x) => x === "#"))) {
-    newGrid = newGrid.map((z) => [...z, Array(xSize).fill(".")]);
-    ySize = newGrid[0].length;
-  }
-
-  // expand z down
-  if (newGrid[0].some((x) => x.some((c) => c === "#"))) {
-    const newLayer = Array(ySize);
-    for (let i = 0; i < ySize; i++) {
-      newLayer[i] = Array(xSize).fill(".");
-    }
-    newGrid.splice(0, 0, newLayer);
-  }
-
-  // expand z up
-  const maxZ = newGrid.length;
-  if (newGrid[maxZ - 1].some((x) => x.some((c) => c === "#"))) {
-    const newLayer = Array(ySize);
-    for (let i = 0; i < ySize; i++) {
-      newLayer[i] = Array(xSize).fill(".");
-    }
-    newGrid.splice(maxZ, 0, newLayer);
-  }
-  return newGrid;
-};
-
-const getAdjacents = (grid: Grid, z: number, y: number, x: number) => {
-  const deltas: [number, number, number][] = [
-    [-1, -1, -1],
-    [-1, -1, 0],
-    [-1, -1, 1],
-    [-1, 0, -1],
-    [-1, 0, 0],
-    [-1, 0, 1],
-    [-1, 1, -1],
-    [-1, 1, 0],
-    [-1, 1, 1],
-    [0, -1, -1],
-    [0, -1, 0],
-    [0, -1, 1],
-    [0, 0, -1],
-    // [0, 0, 0],
-    [0, 0, 1],
-    [0, 1, -1],
-    [0, 1, 0],
-    [0, 1, 1],
-    [1, -1, -1],
-    [1, -1, 0],
-    [1, -1, 1],
-    [1, 0, -1],
-    [1, 0, 0],
-    [1, 0, 1],
-    [1, 1, -1],
-    [1, 1, 0],
-    [1, 1, 1],
-  ];
-  const found = deltas.filter(([dz, dy, dx]) => {
-    return grid[z + dz]?.[y + dy]?.[x + dx] === "#";
-  });
-  // console.log(z, y, x, found);
-  return found.length;
-};
-
-const process = (input: string[], cycles: number = 6) => {
-  let grid: Grid = [input.map((line) => line.split(""))];
-  // console.log(`Initial grid:`);
-  // prettyPrint(grid);
-  for (let i = 0; i < cycles; i++) {
-    const newGrid = cloneAndExpand(grid);
-    const newGridClone = clone(newGrid);
-    const maxZ = newGrid.length;
-    const maxY = newGrid[0].length;
-    const maxX = newGrid[0][0].length;
-    for (let z = 0; z < maxZ; z++) {
-      for (let y = 0; y < maxY; y++) {
-        for (let x = 0; x < maxX; x++) {
-          try {
-            const g = newGrid[z][y][x];
-            const a = getAdjacents(newGrid, z, y, x);
-            if (g === "#" && a !== 2 && a !== 3) {
-              newGridClone[z][y][x] = ".";
-            } else if (g === "." && a === 3) {
-              newGridClone[z][y][x] = "#";
-            }
-          } catch (e) {
-            console.log(newGrid);
-            console.log(z, y, x);
-            throw e;
-          }
-        }
-      }
-    }
-    grid = newGridClone;
-    // console.log(`After cycle ${i + 1}:`);
-    // prettyPrint(grid);
-    // flatPrint(grid);
-  }
-  return grid.reduce(
-    (zsum, y) =>
-      zsum + y.reduce((ysum, x) => ysum + x.filter((c) => c === "#").length, 0),
-    0
-  );
-};
-
-const splitLen = <T>(a: T[], len: number): T[][] => {
-  let result: T[][] = [];
-  const ac = a.slice();
-  while (ac.length > 0) {
-    result.push(ac.splice(0, len));
-  }
-  return result;
-};
-
-const process2 = (input: string[], dimensions: number, cycles: number) => {
+const process = (input: string[], dimensions: number, cycles: number) => {
   let grid = new HyperGrid<boolean>(dimensions, false);
 
   // Initialise with 2-dimensional data;
@@ -401,7 +258,7 @@ const process2 = (input: string[], dimensions: number, cycles: number) => {
     const points = getDimensionalRange(grid.getDimensions(), min, max);
     points.forEach((p) => {
       const state = grid.get(p);
-      const adjacents = grid.countAdjacents(p, (c) => c === true);
+      const adjacents = grid.countAdjacents(p, (c) => c === true, 4);
       if (state === true && adjacents !== 2 && adjacents !== 3) {
         newGrid.set(p, false);
       } else if (state === false && adjacents === 3) {
@@ -419,34 +276,21 @@ const process2 = (input: string[], dimensions: number, cycles: number) => {
 
 const solution: Solution = async () => {
   const input = await getInput;
-  return process(input, 6);
+  return process(input, 3, 6);
 };
 
 solution.tests = async () => {
   const testInput = `.#.
 ..#
 ###`.split("\n");
-
-  /*
-  console.log("");
-  await expect(() => process(testInput, 1), 11);
-  await expect(() => process2(testInput, 3, 1), 11);
-  */
-
-  console.log("");
-  await expect(() => process(testInput, 2), 21);
-  await expect(() => process2(testInput, 3, 2), 21);
-
-  console.log("");
-  await expect(() => process(testInput, 6), 112);
-  await expect(() => process2(testInput, 3, 6), 112);
-
-  await expect(() => process2(testInput, 4, 6), 848);
+  await expect(() => process(testInput, 3, 2), 21);
+  await expect(() => process(testInput, 3, 6), 112);
+  await expect(() => process(testInput, 4, 6), 848);
 };
 
 solution.partTwo = async () => {
   const input = await getInput;
-  return process2(input, 4, 6);
+  return process(input, 4, 6);
 };
 
 solution.inputs = [getInput];
